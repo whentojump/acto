8bcff04a0086a3a14b6932f1a27daefcd71f02fc
Wentao Zhang <zhangwt1997@gmail.com>
2024-03-17 23:41:55 -0500

    erh

--- data/percona-server-mongodb-operator/custom_mapping.py
+++ data/percona-server-mongodb-operator/custom_mapping.py
@@ -5,14 +5,14 @@ from acto.input.property_attribute import (
 
 tag_property_attribute(["spec", "pmm"], PropertyAttribute.Prune)
 tag_property_attribute(["spec", "crVersion"], PropertyAttribute.Prune)
-tag_property_attribute(["spec", "mongod", "security"], PropertyAttribute.Prune)
-tag_property_attribute(
-    ["spec", "mongod", "setParameter"], PropertyAttribute.Prune
-)
-tag_property_attribute(
-    ["spec", "mongod", "replication"], PropertyAttribute.Prune
-)
-tag_property_attribute(
-    ["spec", "mongod", "operationProfiling"], PropertyAttribute.Prune
-)
-tag_property_attribute(["spec", "mongod", "storage"], PropertyAttribute.Mapped)
+# tag_property_attribute(["spec", "mongod", "security"], PropertyAttribute.Prune)
+# tag_property_attribute(
+#     ["spec", "mongod", "setParameter"], PropertyAttribute.Prune
+# )
+# tag_property_attribute(
+#     ["spec", "mongod", "replication"], PropertyAttribute.Prune
+# )
+# tag_property_attribute(
+#     ["spec", "mongod", "operationProfiling"], PropertyAttribute.Prune
+# )
+# tag_property_attribute(["spec", "mongod", "storage"], PropertyAttribute.Mapped)
